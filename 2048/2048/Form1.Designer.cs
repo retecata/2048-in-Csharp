03bab706982edabbd08bb42b79189f203625bc64
Catalina <catalinarete@hotmail.com>
2017-11-30 16:52:13 +0000

    Added old project from highschool

---
+++ 2048/2048/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace _2048
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Scor = new System.Windows.Forms.Button();
+            this.gata = new System.Windows.Forms.RichTextBox();
+            this.Highscore = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Scor
+            // 
+            this.Scor.BackColor = System.Drawing.Color.PeachPuff;
+            this.Scor.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Scor.Location = new System.Drawing.Point(216, 31);
+            this.Scor.Name = "Scor";
+            this.Scor.Size = new System.Drawing.Size(130, 59);
+            this.Scor.TabIndex = 0;
+            this.Scor.Text = "0";
+            this.Scor.UseVisualStyleBackColor = false;
+            // 
+            // gata
+            // 
+            this.gata.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gata.Location = new System.Drawing.Point(31, 215);
+            this.gata.Name = "gata";
+            this.gata.Size = new System.Drawing.Size(325, 91);
+            this.gata.TabIndex = 1;
+            this.gata.Text = " Ai pierdut! Incepe un joc nou";
+            this.gata.TextChanged += new System.EventHandler(this.gata_TextChanged);
+            // 
+            // Highscore
+            // 
+            this.Highscore.BackColor = System.Drawing.Color.PeachPuff;
+            this.Highscore.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Highscore.Location = new System.Drawing.Point(31, 31);
+            this.Highscore.Name = "Highscore";
+            this.Highscore.Size = new System.Drawing.Size(130, 59);
+            this.Highscore.TabIndex = 2;
+            this.Highscore.Text = "0";
+            this.Highscore.UseVisualStyleBackColor = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(393, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.newGameToolStripMenuItem.Text = "New game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 448);
+            this.Controls.Add(this.Highscore);
+            this.Controls.Add(this.gata);
+            this.Controls.Add(this.Scor);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Scor;
+        private System.Windows.Forms.RichTextBox gata;
+        private System.Windows.Forms.Button Highscore;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+
+    }
+}
+
---
+++ 2048/2048/Form1.cs
@@ -0,0 +1,495 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace _2048
+{
+    public partial class Form1 : Form
+    {
+        int n = 3;
+        int rez;
+        Boolean mutare = false;
+        Button[,] buttonMatrix = new Button[4, 4];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int sus = 30, jos = 100;
+            gata.Hide();
+            int i, j;
+            for (i = 0; i <= n; i++)
+            {
+                for (j = 0; j <= n; j++)
+                {
+
+
+                    buttonMatrix[i, j] = new Button();
+
+                    buttonMatrix[i, j].Height = 80;
+                    buttonMatrix[i, j].Width = 80;
+                    buttonMatrix[i, j].Text = "";
+                    buttonMatrix[i, j].FlatStyle = FlatStyle.Flat;
+                    buttonMatrix[i, j].FlatAppearance.BorderColor = Color.Black;
+                    buttonMatrix[i, j].FlatAppearance.BorderSize = 1;
+                    buttonMatrix[i, j].Location = new Point(sus, jos);
+                    buttonMatrix[i, j].Font = new Font("Georgia", 16);
+                    ;
+
+                    sus += 80;
+
+
+
+                    this.Controls.Add(buttonMatrix[i, j]);
+
+                }
+                jos += 80;
+                sus = 30;
+
+            }
+            Random rnd = new Random();
+            int x = rnd.Next(0, 3);
+            int y = rnd.Next(0, 3);
+            buttonMatrix[x, y].Text = "2";
+            changecolor();
+            Highscore.Text = File.ReadAllText(Path.GetFullPath("Highscore.txt"));
+
+
+
+        }
+        private void gameover()
+        {
+            int ok = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    if (buttonMatrix[i,j].Text == "")
+                    {
+                        ok = 1;
+                    }
+                }
+            }
+            if (ok == 0)
+            {
+                Boolean gameover = true;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (buttonMatrix[i, j].Text == buttonMatrix[i + 1, j].Text ||
+                            buttonMatrix[i, j].Text == buttonMatrix[i, j + 1].Text)
+                            gameover = false;
+                    }
+                }
+                if (buttonMatrix[3, 3].Text == buttonMatrix[3, 2].Text || buttonMatrix[3, 3].Text == buttonMatrix[2, 3].Text)
+                {
+                    gameover = false;
+                }
+
+                if (gameover == true)
+                {
+
+
+                    gata.Show();
+                    int s = Convert.ToInt32(Scor.Text);
+                    string value1 = File.ReadAllText(Path.GetFullPath("Highscore.txt"));
+                    if (s > Convert.ToInt32(value1))
+                    {
+                        File.WriteAllText(Path.GetFullPath("Highscore.txt"), String.Empty);
+                        File.WriteAllText(Path.GetFullPath("Highscore.txt"), s.ToString());
+                        Highscore.Text = s.ToString();
+                    }
+                    Scor.Text = "0";
+
+
+                }
+            }
+        }
+        public void nrnou()
+        {
+           
+            Random rnd = new Random();
+            int x, y;
+            if (mutare == true)
+            {
+                do
+                {
+                    x = rnd.Next(0, 4);
+                    y = rnd.Next(0, 4);
+                } while (buttonMatrix[x, y].Text != "");
+                int greu;
+                greu = rnd.Next(0, 5);
+                if (greu % 2 == 0)
+                {
+                    buttonMatrix[x, y].Text = "2";
+
+                }
+                else
+                {
+                    buttonMatrix[x, y].Text = "4";
+
+                }
+            }
+            changecolor();
+        }
+        private void jos()
+        {
+           
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int urm = i;
+
+
+                for (int j = 0; j <= n; j++)
+                {
+
+                    
+                    
+                        for (urm = i; urm < n; urm++)
+                        {
+                             if (buttonMatrix[urm + 1,j].Text == ""&& buttonMatrix[urm, j].Text != "")
+                   
+                            {
+                                buttonMatrix[urm + 1, j].Text = buttonMatrix[urm, j].Text;
+                                buttonMatrix[urm, j].Text = "";
+                                changecolor();
+                                mutare = true;
+                            }
+
+                        
+                    }
+
+                }
+            }
+        }
+        private void sus()
+        {
+            
+            for (int i = 1; i < n; i++)
+            {
+
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int urm = n; urm > 0; urm--)
+                    {
+                        
+                            if (buttonMatrix[urm - 1, j].Text == ""&& buttonMatrix[urm, j].Text != "")
+                            {
+                                buttonMatrix[urm - 1, j].Text = buttonMatrix[urm, j].Text;
+                                buttonMatrix[urm, j].Text = "";
+                                mutare = true;
+                                changecolor();
+                            }
+                        
+                    }
+
+                }
+
+            }
+        }
+        private void dreapta()
+        {
+           
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = n - 1; j >= 0; j--)
+                {
+                    
+                        for (int urm = j; urm < n; urm++)
+                        {
+                            if (buttonMatrix[i, urm + 1].Text == ""&& buttonMatrix[i, urm].Text != "")
+                            {
+                                buttonMatrix[i, urm + 1].Text = buttonMatrix[i, urm].Text;
+                                buttonMatrix[i, urm].Text = "";
+                                changecolor();
+                                mutare = true;
+
+                            }
+                        }
+                    
+                }
+            }
+        }
+        private void stanga()
+        {
+           
+            for (int i = 0; i <= n; i++)
+            {
+
+                for (int j = 1; j <= n; j++)
+                {
+                    
+                    for (int urm = j; urm > 0; urm--)
+                    {
+                        if (buttonMatrix[i, urm - 1].Text == "" && buttonMatrix[i, urm].Text != "")
+                        {
+                            buttonMatrix[i, urm - 1].Text = buttonMatrix[i, urm].Text;
+                            buttonMatrix[i, urm].Text = "";
+                            changecolor();
+                            mutare = true;
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                   
+                case Keys.Down:
+                    mutare = false;
+                    jos();
+                    gameover();
+                    for (int i = n - 1; i >= 0; i--)
+                    {
+                        int urm = i;
+
+                        for (int j = 0; j <= n; j++)
+                        {
+
+                            if (buttonMatrix[i + 1, j].Text == buttonMatrix[i, j].Text)
+                            {
+
+
+                                if (buttonMatrix[i, j].Text != "")
+                                {
+                                    int s = Convert.ToInt32(buttonMatrix[i + 1, j].Text);
+                                    int m = Convert.ToInt32(buttonMatrix[i, j].Text);
+
+
+                                    s = s + m;
+                                    buttonMatrix[i + 1, j].Text = s.ToString();
+                                    buttonMatrix[i, j].Text = "";
+                                    changecolor();
+                                     rez = Convert.ToInt32(Scor.Text) + Convert.ToInt32(buttonMatrix[i+1, j].Text);
+                                    Scor.Text = rez.ToString();
+                                    mutare = true;
+                                }
+
+                            }
+
+                        }
+                    }
+                    jos();
+                    if(mutare==true)
+                    nrnou();
+                    
+
+                    break;
+                case Keys.Up:
+                    mutare = false;
+                    sus();
+                    gameover();
+                    for (int i = 1; i <= n; i++)
+                    {
+
+                        for (int j = 0; j <= n; j++)
+                        {
+
+
+                            if (buttonMatrix[i - 1, j].Text == buttonMatrix[i, j].Text)
+                            {
+
+
+                                if (buttonMatrix[i, j].Text != "")
+                                {
+                                    int s = Convert.ToInt32(buttonMatrix[i - 1, j].Text);
+                                    int m = Convert.ToInt32(buttonMatrix[i, j].Text);
+
+
+                                    s = s + m;
+                                    buttonMatrix[i - 1, j].Text = s.ToString();
+                                    buttonMatrix[i, j].Text = "";
+                                    rez= Convert.ToInt32(Scor.Text) + Convert.ToInt32(buttonMatrix[i - 1, j].Text);
+                                    Scor.Text = rez.ToString();
+                                    mutare = true;
+                                }
+
+                            }
+                        }
+
+                    }
+                    sus();
+
+                    if(mutare==true)
+                    nrnou();
+                    break;
+                case Keys.Right:
+                    mutare = false;
+                    dreapta();
+                    gameover();
+                    for (int i = 0; i <= n; i++)
+                    {
+                        for (int j = n - 1; j >= 0; j--)
+                        {
+                            if (buttonMatrix[i, j + 1].Text == buttonMatrix[i, j].Text)
+                            {
+
+
+                                if (buttonMatrix[i, j].Text != "")
+                                {
+                                    int s = Convert.ToInt32(buttonMatrix[i, j + 1].Text);
+                                    int m = Convert.ToInt32(buttonMatrix[i, j].Text);
+
+
+                                    s = s + m;
+                                    buttonMatrix[i, j + 1].Text = s.ToString();
+                                    buttonMatrix[i, j].Text = "";
+                                    changecolor();
+                                     rez = Convert.ToInt32(Scor.Text) + Convert.ToInt32(buttonMatrix[i , j+1].Text);
+                                    Scor.Text = rez.ToString();
+                                    mutare = true;
+                                }
+
+                            }
+                        }
+                    }
+                    dreapta();
+                    nrnou();
+                    break;
+                case Keys.Left:
+                    mutare = false;
+                    stanga();
+                    gameover();
+                    for (int i = 0; i <= n; i++)
+                    {
+                        for (int j = 1; j <= n; j++)
+                        {
+
+
+                            if (buttonMatrix[i, j - 1].Text == buttonMatrix[i, j].Text)
+                            {
+                                if (buttonMatrix[i, j].Text != "")
+                                {
+                                    int s = Convert.ToInt32(buttonMatrix[i, j - 1].Text);
+                                    int m = Convert.ToInt32(buttonMatrix[i, j].Text);
+
+
+                                    s = s + m;
+                                    buttonMatrix[i, j - 1].Text = s.ToString();
+                                    buttonMatrix[i, j].Text = "";
+                                    changecolor();
+                                     rez = Convert.ToInt32(Scor.Text) + Convert.ToInt32(buttonMatrix[i , j-1].Text);
+                                    Scor.Text = rez.ToString();
+                                    mutare = true;
+                                }
+
+                            }
+                        }
+                    }
+
+                    stanga();
+                    if(mutare==true)
+                    nrnou();
+                    
+                    break;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        private void changecolor()
+        {
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    if (buttonMatrix[i, j].Text == "")
+                    {
+                        buttonMatrix[i, j].BackColor = Color.White;
+                    }
+                    if (buttonMatrix[i, j].Text == "2")
+                    {
+                        buttonMatrix[i, j].BackColor = Color.LightYellow;
+                    }
+                    if (buttonMatrix[i, j].Text == "4")
+                    {
+                        buttonMatrix[i, j].BackColor = Color.PeachPuff;
+                    }
+                    if (buttonMatrix[i, j].Text == "8")
+                    {
+                        buttonMatrix[i, j].BackColor = Color.Bisque;
+                    }
+                    if (buttonMatrix[i, j].Text == "16")
+                    {
+                        buttonMatrix[i, j].BackColor = Color.BurlyWood;
+                    }
+                    if (buttonMatrix[i, j].Text == "32")
+                    {
+                        buttonMatrix[i, j].BackColor = Color.LightPink;
+                    }
+                    if (buttonMatrix[i, j].Text == "64")
+                    {
+                        buttonMatrix[i, j].BackColor = Color.RosyBrown;
+                    }
+                    if (buttonMatrix[i, j].Text == "128")
+                    {
+                        buttonMatrix[i, j].BackColor = Color.IndianRed;
+                    }
+                    if (buttonMatrix[i, j].Text == "256")
+                    {
+                        buttonMatrix[i, j].BackColor = Color.Plum;
+                    }
+                    if (buttonMatrix[i, j].Text == "512")
+                    {
+                        buttonMatrix[i, j].BackColor = Color.SandyBrown;
+                    }
+                }
+            }
+
+        }
+
+     
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    buttonMatrix[i, j].Text = "";
+                    buttonMatrix[i, j].BackColor = Color.White;
+                }
+            }
+            gata.Hide();
+            int s = Convert.ToInt32(Scor.Text);
+            string value1 = File.ReadAllText(Path.GetFullPath("Highscore.txt"));
+            if(s>Convert.ToInt32(value1))
+            {
+                File.WriteAllText(Path.GetFullPath("Highscore.txt"), String.Empty);
+                File.WriteAllText(Path.GetFullPath("Highscore.txt"), s.ToString());
+                Highscore.Text = s.ToString();
+            }
+            Scor.Text = "0";
+
+
+            Random rnd = new Random();
+            int x = rnd.Next(0, 3);
+            int y = rnd.Next(0, 3);
+            buttonMatrix[x, y].Text = "2";
+            changecolor();
+        }
+
+        private void gata_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
